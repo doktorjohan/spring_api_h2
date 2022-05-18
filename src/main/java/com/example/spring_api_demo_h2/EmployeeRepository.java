7de80ac1977a15171f9a42666833563ff948149d
johanhollak <johanhollak1@gmail.com>
2022-05-18 13:22:12 +0300

    improved wording

--- src/main/java/com/example/spring_api_demo_h2/EmployeeRepository.java
+++ src/main/java/com/example/spring_api_demo_h2/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.example.spring_api_demo_h2;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Spring Boot shenanigan, provides easy access to database
+ * Java Persistence API shenanigan, provides easy access to database
  */
 public interface EmployeeRepository extends JpaRepository<Employee, Long>
 {
